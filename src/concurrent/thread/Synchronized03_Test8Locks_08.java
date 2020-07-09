541a03ddc30fe8f8e8b7af50efdd61154da5cda8
hngydxlinjing <1259478950@qq.com>
2020-07-09 22:53:50 +0800

    提交线程8锁，线程安全相关案例

---
+++ src/concurrent/thread/Synchronized03_Test8Locks_08.java
@@ -0,0 +1,39 @@
+package threadtest;
+
+/**
+ * @version V1.0
+ * @author: zouwh
+ * @description: 认识synchronized关键字
+ * @date: 2020/7/5 3:21 PM
+ * 锁静态方法等于锁类，锁同一个类相当于互斥锁
+ * 结果：1 1s 1 ,2 1s 1
+ */
+public class Synchronized03_Test8Locks_08 {
+
+    public static void main(String[] args) {
+        Number08 number1 = new Number08();
+        Number08 number2 = new Number08();
+        Thread thread1 = new Thread(()->number1.a());
+        Thread thread2 = new Thread(()->number2.b());
+        thread1.start();
+        thread2.start();
+    }
+}
+
+
+class Number08{
+    public static synchronized void a(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("1....");
+    }
+
+    public static  synchronized void b(){
+
+        System.out.println("2....");
+    }
+
+}
---
+++ src/concurrent/thread/ThreadSafe_MemberVariable.java
@@ -0,0 +1,83 @@
+package concurrent.thread;
+
+/**
+ * @version V1.0
+ * @author: zouwh
+ * @description: 线程安全问题示例
+ * @date: 2020/7/5 3:16 PM
+ * 一个线程对i变量加5000此，另一个线程对i变量减5000次，但结果不是0
+ */
+public class ThreadSafe_MemberVariable {
+
+    public static void main(String[] args) throws Exception {
+        CounterUnSafe counter = new CounterUnSafe();
+      //  CounterSafe counter = new CounterSafe();
+        Thread thread1 = new Thread(()->{
+            for(int j=0;j<5000;j++){
+                counter.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(()->{
+            for(int j=0;j<5000;j++){
+                counter.decrement();
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
+        System.out.println(counter.getCount());
+    }
+
+}
+
+
+class CounterUnSafe {
+    private   int count=0;
+    public void increment(){
+        count++;
+    }
+    public void decrement(){
+        count--;
+    }
+    public int getCount(){
+        return count;
+    }
+
+}
+
+/**
+ * @author: zouwh
+ * @description:
+ * @date:   2020/7/5
+ * @version V1.0
+ * 线程安全： 即多个线程调用同一个实例的某个方法时，是线程安全的
+ * 误区：每个方法是线程安全的，所有方法组合在一起并不一定是线程安全的
+ */
+class CounterSafe {
+    private  int count=0;
+
+    public void increment(){
+        synchronized (this){
+            this.count++;
+        }
+    }
+
+    public void decrement(){
+        synchronized (this){
+            this.count--;
+        }
+    }
+
+    public int getCount(){
+        synchronized (this){
+            return this.count;
+        }
+    }
+
+}
+
+
+
