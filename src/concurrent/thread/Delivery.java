9a996fc816ec58b10efff00260f36521aad05804
zouwh <zouwh@ztzqzg.com>
2020-07-12 10:48:10 +0800

    完善点外卖示例

--- src/concurrent/thread/Delivery.java
+++ src/concurrent/thread/Delivery.java
@@ -18,9 +18,8 @@ public class Delivery {
     public static void main(String[] args) {
         OrderBoxes orderBoxes = new OrderBoxes();
         new Person().order("粥",orderBoxes).start();
-        int orderNo = orderBoxes.generateId();
-
-
+        TakeOutBoy takeOutBoy =orderBoxes.distributTakeOutBoy();
+        takeOutBoy.delivery();
 
     }
 
@@ -61,15 +60,23 @@ class Person extends Thread{
 
     @Override
     public void run(){
-        log.debug("点了一份{}",this.orderName);
-        int orderId  = this.plateForm.receiveOrder();
-        Food food = get(orderId);
+        log.debug("点一份{}",this.orderName);
+        int orderId = order(this.orderName);
+        Food food = waitFood(orderId);
         log.debug("拿到外卖{}", food.getFoodName());
 
     }
 
-    public Food get(int id){
-        return (Food) this.plateForm.get(id);
+    public int order(String orderName){
+        int orderId  = this.plateForm.createOrder(orderName);
+        return orderId;
+    }
+
+    public Food waitFood(int id){
+        GuardedObject2 guardedObject2= this.plateForm.getGuardedObject2ById(id);
+        Food food = (Food)guardedObject2.get();
+        return food;
+
     }
 
 }
@@ -81,41 +88,72 @@ class OrderBoxes{
     private static int id = 1;
 
     public synchronized static int generateId(){
-        return id++;
+         return id++;
     }
 
-    public int  receiveOrder(){
+    public int createOrder(String orderName){
         GuardedObject2  guardedObject2  = new GuardedObject2(OrderBoxes.generateId());
+        guardedObject2.setGoods(orderName);
         boxex.put(guardedObject2.getId(),guardedObject2);
         return guardedObject2.getId();
     }
 
-    public Food get(int id){
+    public GuardedObject2 getGuardedObject2ById(int id){
         GuardedObject2  guardedObject2  = boxex.get(id);
-        Food food = (Food)guardedObject2.get();
-        return food;
+        return guardedObject2;
+    }
+
+    public TakeOutBoy distributTakeOutBoy() {
+        GuardedObject2  guardedObject2  = getGuardedObject2ById(id);
+        return new TakeOutBoy(id,this,((Food)guardedObject2.getGoods()).getFoodName());
     }
 }
 
 /*
-* 外卖xiaoge
+* 外卖小哥
 */
+@Slf4j
 class TakeOutBoy{
 
+    private OrderBoxes plateForm;
+    private int orderId;
+    private String foodName;
+
+
+    TakeOutBoy(int  orderId,OrderBoxes plateForm,String foodName){
+        this.plateForm = plateForm;
+        this.orderId = orderId;
+        this.foodName = foodName;
+    }
+
+    public void delivery(){
+        Food  food = new Food(this.foodName);
+        GuardedObject2 guardedObject2 =  this.plateForm.getGuardedObject2ById(this.orderId);
+        synchronized (this){
+            try {
+                guardedObject2.setResult(food);
+                Thread.sleep(1000);
+                log.debug("送外卖{}成功！",((Food)guardedObject2.getGoods()).getFoodName()) ;
+                this.notifyAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                guardedObject2.setResult("其他突发事件，未及时送达！");;
+            }
+        }
+    }
 
 }
 
 class    Food{
     private String foodName ;
 
-    public String getFoodName() {
-        return foodName;
-    }
-
-    public void setFoodName(String foodName) {
+    public Food(String foodName) {
         this.foodName = foodName;
     }
 
+    public String getFoodName() {
+        return foodName;
+    }
 
 }
 
@@ -123,6 +161,7 @@ class    Food{
 @Slf4j
  class GuardedObject2{
     private int id;
+    private Object goods;
     private Object result;
 
     public GuardedObject2(int id) {
@@ -133,8 +172,20 @@ class    Food{
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public Object getGoods() {
+        return goods;
+    }
+
+    public void setGoods(Object goods) {
+        this.goods = goods;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
     }
 
     public Object get(){
@@ -153,17 +204,6 @@ class    Food{
 
     }
 
-    public void delivery(Object object){
-        synchronized (this){
-            try {
-                Thread.sleep(1000);
-                this.result =object;
-                log.debug("送外卖成功！") ;
-                this.notifyAll();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                this.result = "其他突发事件，未及时送达！";
-            }
-        }
-    }
+
+
 }
